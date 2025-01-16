ba10693992775a39baaa610c288113d4ebb96345
Marcel Hanselmann <hanselmann48@gmail.com>
2025-01-16 17:57:41 +0100

    add axum

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world! I'm the backend");
+use axum::Router;
+use axum::routing::get;
+
+#[tokio::main]
+
+async fn main() {
+    let app = Router::new()
+        .route("/", get(hello));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello() -> &'static str {
+    "Hello, axum! I'm the backend"
 }
