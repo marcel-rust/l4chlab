c4292725ab451f0bc9872e113e2d4de31cd28faf
Marcel Hanselmann <hanselmann48@gmail.com>
2025-01-16 18:08:49 +0100

    add serde_json

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,15 +1,26 @@
-use axum::Router;
+use axum::http::StatusCode;
+use axum::{Json, Router};
 use axum::routing::get;
+use serde::Serialize;
 
 #[tokio::main]
 
 async fn main() {
     let app = Router::new()
-        .route("/", get(hello));
+        .route("/", get(hello_json));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn hello() -> &'static str {
-    "Hello, axum! I'm the backend"
+#[derive(Serialize)]
+struct Response {
+    message: &'static str,
+}
+
+async fn hello_json() -> (StatusCode, Json<Response>) {
+    let response = Response {
+        message: "Hello, axum! I'm the backend",
+    };
+    
+    (StatusCode::OK, Json(response))
 }
