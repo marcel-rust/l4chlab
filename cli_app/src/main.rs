45453c15f0d958c65c44337b3fa990f710d233a7
Marcel Hanselmann <hanselmann48@gmail.com>
2025-01-17 17:16:20 +0100

    add commands

---
+++ cli_app/src/main.rs
@@ -0,0 +1,13 @@
+use clap::{Arg, Command};
+use cli_app::commands;
+
+pub fn main() -> anyhow::Result<()> {
+
+    let mut command = Command::new("Sample CLI application");
+    command = commands::configure(command);
+    
+    let matches = command.get_matches();
+    commands::handle(&matches)?;
+
+    Ok(())
+}
